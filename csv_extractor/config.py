d876543d16efb6645fa2036864a52d7f94f4b288
kevin.peng <zhenghongpeng@yahoo.com>
2023-02-15 10:53:47 -0600

    well5832

--- csv_extractor/config.py
+++ csv_extractor/config.py
@@ -1,14 +1,21 @@
 
 from dataclasses import dataclass
+from typing import List
 
-from cognite.extractorutils.configtools import BaseConfig, StateStoreConfig
+from cognite.extractorutils.configtools import BaseConfig, StateStoreConfig, RawDestinationConfig
 
 
 @dataclass
 class ExtractorConfig:
     state_store: StateStoreConfig = StateStoreConfig()
 
+@dataclass
+class FileConfig:
+    path: str
+    key_column: str
+    destination: RawDestinationConfig
 
 @dataclass
 class Config(BaseConfig):
+    files: List[FileConfig]
     extractor: ExtractorConfig = ExtractorConfig()
--- csv_extractor/extractor.py
+++ csv_extractor/extractor.py
@@ -1,11 +1,40 @@
-
+import logging
+import os
+import stat
+from csv import DictReader
 from threading import Event
 
 from cognite.client import CogniteClient
+from cognite.client.data_classes import Row
 from cognite.extractorutils.statestore import AbstractStateStore
+from cognite.extractorutils.uploader import RawUploadQueue
 
-from csv_extractor.config import Config
+from csv_extractor.config import Config, FileConfig
 
+logger = logging.getLogger()
 
+def extract_file(file: FileConfig, queue: RawUploadQueue) -> None:
+
+    logger.info(f"Extracting {file.path}")
+    with open(file.path) as infile:
+        reader = DictReader(infile)
+
+        for row in reader:
+            queue.add_to_upload_queue(
+                database=file.destination.database,
+                table=file.destination.table,
+                raw_row=Row(key=row[file.key_column], columns=row)
+            )
 def run_extractor(cognite: CogniteClient, states: AbstractStateStore, config: Config, stop_event: Event) -> None:
-    print("Hello, world!")
+
+    with RawUploadQueue(cdf_client=cognite, max_queue_size=100_000) as queue:
+        for file in config.files:
+            if stop_event.is_set():
+                break
+            previous_version = states.get_state(file.path)[1] or 0
+            new_version = os.stat(file.path)[stat.ST_MTIME]
+            if new_version > previous_version:
+                extract_file(file, queue)
+                states.expand_state(file.path, high=new_version)
+            else:
+                logger.info(f"{file.path} has not changed, skipping")
